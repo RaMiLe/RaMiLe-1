9e47265bce96e78224b89880d5db404b08c2f181
ramiltugushev <32484335+ramiltugushev@users.noreply.github.com>
2017-12-08 14:22:04 +0400

    Update createtable.php

---
+++ createtable.php
@@ -0,0 +1,22 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:rom97.database.windows.net,1433; Database = Rom", "ramiltugushev", "RaMiLe");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ramiltugushev@rom97", "pwd" => "{your_password_here}", "Database" => "Rom", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:rom97.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
