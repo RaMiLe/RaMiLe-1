851bf151b94bdf6642be2a742823fb4bc6ba85e7
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-10 21:27:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,33 +35,7 @@ name="email" id="email"/></br>
       <input type="submit" 
 name="submit" value="Submit" />
    
-$host = "localhost\sqlexpress";
-$user = "user name";
-$pwd = "password";
-$db = "registration";
-// Connect to database.
-try {
-    $conn = new PDO
-( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute
-( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch(Exception $e){
-    die(var_dump($e));
-    $sql_select = "SELECT * FROM registration_tbl";
-$stmt = $conn->query($sql_select);
-$registrants = $stmt->fetchAll(); 
-if(count($registrants) > 0) {
-    echo "<h2>People who are registered:</h2>";
-    echo "<table>";
-    echo "<tr><th>Name</th>";
-    echo "<th>Email</th>";
-    echo "<th>Date</th></tr>";
-    foreach($registrants as $registrant) {
-        echo "<tr><td>".$registrant['name']."</td>";
-        echo "<td>".$registrant['email']."</td>";
-        echo "<td>".$registrant['date']."</td></tr>";
-    }
+
     echo "</table>";
 } else {
     echo "<h3>No one is currently registered.</h3>";
